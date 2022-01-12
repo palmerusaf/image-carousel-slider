d24a99095646850bfd2292d98a1f5776057d2725
Branden Palmer <brandenpalmer08@gmail.com>
2022-01-12 13:36:01 +0700

    finish dot cycle test

--- src/dot-nav-field/dot-nav-field-tests.js
+++ src/dot-nav-field/dot-nav-field-tests.js
@@ -6,3 +6,19 @@ pubsub.subscribe("changeActiveIndex", console.log);
 document.body.appendChild(makeDotNavField(4));
 
 pubsub.publish("changeActiveIndex", 2);
+
+// Previous button press test
+const previousButton = document.createElement("button");
+previousButton.textContent = "previous";
+previousButton.addEventListener("click", () => {
+  pubsub.publish("previousButtonPressed");
+});
+document.body.appendChild(previousButton);
+
+// next button press test
+const nextButton = document.createElement("button");
+nextButton.textContent = "next";
+nextButton.addEventListener("click", () => {
+  pubsub.publish("nextButtonPressed");
+});
+document.body.appendChild(nextButton);
