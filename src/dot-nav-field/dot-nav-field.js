92da6ff87caa5cf48d7e8f0090e6dde82487e0de
Branden Palmer <brandenpalmer08@gmail.com>
2022-01-12 13:35:28 +0700

    fix bug with cycle previous dot

--- src/dot-nav-field/dot-nav-field.js
+++ src/dot-nav-field/dot-nav-field.js
@@ -57,7 +57,7 @@ function _cycleActiveStatusToNextDot() {
     pubsub.publish("changeActiveIndex", nextDotIndex);
   }
 }
-setInterval(_cycleActiveStatusToNextDot, 5000);
+// setInterval(_cycleActiveStatusToNextDot, 5000);
 pubsub.subscribe("nextButtonPressed", _cycleActiveStatusToNextDot);
 
 function _cycleActiveStatusToPreviousDot() {
@@ -70,5 +70,5 @@ function _cycleActiveStatusToPreviousDot() {
   } else {
     pubsub.publish("changeActiveIndex", previousDotIndex);
   }
-  pubsub.subscribe("previousButtonPressed", _cycleActiveStatusToPreviousDot);
 }
+pubsub.subscribe("previousButtonPressed", _cycleActiveStatusToPreviousDot);
