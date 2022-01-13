15d51cba5c52b60390d5c09576650f45c6295981
Branden Palmer <brandenpalmer08@gmail.com>
2022-01-13 12:02:29 +0700

    add make arrow button func

---
+++ src/picture-window/picture-window.js
@@ -0,0 +1,41 @@
+import { pubsub } from "../pubsub";
+import "./style.scss";
+import arrow from "./forward-arrow.png";
+
+export function makePictureWindow() {
+  const container = document.createElement("div");
+  container.appendChild(_makeImageNavButtons());
+  container.classList = "picture-window";
+  return container;
+}
+
+function _makeImageNavButtons() {
+  const buttons = document.createDocumentFragment();
+
+  const previousButton = _makeArrowButton();
+  previousButton.classList = "picture-window__previous-button";
+  previousButton.addEventListener("click", () =>
+    pubsub.publish("previousButtonPressed")
+  );
+
+  const nextButton = _makeArrowButton();
+  nextButton.classList = "picture-window__next-button";
+  nextButton.addEventListener("click", () =>
+    pubsub.publish("nextButtonPressed")
+  );
+
+  buttons.appendChild(previousButton);
+  buttons.appendChild(nextButton);
+  return buttons;
+}
+
+export function _makeArrowButton() {
+  const button = document.createElement("button");
+  button.classList = "arrow-button";
+  const arrowImg = document.createElement("img");
+  arrowImg.classList = "arrow-button__arrow";
+  arrowImg.src = arrow;
+  button.appendChild(arrowImg);
+
+  return button;
+}
