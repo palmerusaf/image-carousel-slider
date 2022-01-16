10f85d6b7c2ce3cbde9a3502e5f7375a34d51ef1
Branden Palmer <brandenpalmer08@gmail.com>
2022-01-16 14:36:48 +0700

    refactor picture window

--- src/index-manager/index-manager-test.js
+++ src/index-manager/index-manager-test.js
@@ -3,10 +3,11 @@ import { makeDotNavField } from "../dot-nav-field/dot-nav-field";
 import { makePictureWindow } from "../picture-window/picture-window";
 import { IndexManager } from "./index-manager";
 
-const indexSize = document.querySelectorAll(".slide-image").length;
+const documentPictures = document.querySelectorAll(".slide-image");
+const indexSize = documentPictures.length;
 const container = document.createElement("div");
 document.body.appendChild(container);
 container.classList = "container";
-container.appendChild(makePictureWindow());
+container.appendChild(makePictureWindow(documentPictures));
 container.appendChild(makeDotNavField(indexSize));
 IndexManager.initWithIndexSize(indexSize);
