6d6ebbb7178f64d8f8ae469796132ccd7bef6f12
Branden Palmer <brandenpalmer08@gmail.com>
2022-01-16 16:59:49 +0700

    integrate modules

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
+import { makeDotNavField } from "./dot-nav-field/dot-nav-field";
+import { makePictureWindow } from "./picture-window/picture-window";
+import { IndexManager } from "./index-manager/index-manager";
 import "./image-slider-style.scss";
-import "./index-manager/index-manager-test";
+
+const documentPictures = document.querySelectorAll(".slide-image");
+const indexSize = documentPictures.length;
+const imageSlider = document.querySelector(".image-slider");
+imageSlider.appendChild(makePictureWindow(documentPictures));
+imageSlider.appendChild(makeDotNavField(indexSize));
+IndexManager.initWithIndexSize(indexSize);
