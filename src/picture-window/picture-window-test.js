281522c1b79603ce1995eaff1daa128d6946b158
Branden Palmer <brandenpalmer08@gmail.com>
2022-01-17 14:27:32 +0700

    add files for preview

--- src/picture-window/picture-window-test.js
+++
@@ -1,6 +0,0 @@
-import { makePictureWindow } from "./picture-window";
-import "../dot-nav-field/dot-nav-field-tests";
-
-// initial styling of arrow button
-const container = document.querySelector(".container");
-container.prepend(makePictureWindow());
